a0b4de67a81984c9741d2ffe19635168c41b0c74
David <Nexfortisme@gmail.com>
2024-04-28 14:36:29 -0400

    Removed token refreshing on load

--- src/main.ts
+++ src/main.ts
@@ -7,17 +7,10 @@ import ElementPlus from 'element-plus'
 import App from './App.vue'
 import router from './router'
 
-import { useAuthStore } from './stores/auth'
-
-
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
-// Refreshing token on page load;
-let authStore = useAuthStore()
-authStore.refreshToken();
 
 app.use(ElementPlus)
 
